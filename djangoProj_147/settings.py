392a254ba67777b1c96e63207ae0791fd9165eb1
KoHaKu101 <poou8558@gmail.com>
2023-02-19 17:07:52 +0700

    29/01/23

--- djangoProj_147/settings.py
+++ djangoProj_147/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rg!g+$5n9#w@!!v9#w_negbc=#m=25c#*pl^qv)74-9-_&tncy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
